9e16f0aa5027fffff664134ef86be14aa2466a7e
danielm179 <91908325+danielm179@users.noreply.github.com>
2022-03-07 20:47:44 +0000

    functions

--- README.md
+++ README.md
@@ -8,18 +8,18 @@
 _Replace anything surrounded by the `< >` symbols._
 
 ## SUMMARY:
- < Provide a short description of your program's functionality. No more than a couple sentences. >
+ This program is used to create different kinds of mathematical curves by creating function definitions with the turtle function.
 
 ## GRACE DAYS
-Grace days used for this assignment: < # >
+Grace days used for this assignment: 0
 
-Grace days remaining: < # >/5
+Grace days remaining: 5/5
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
- < What parts of the project you were not able to complete >
+ Getting a complete sin curve to be formed.
 
 ## REFERENCES:
- < List any outside resources used >
+ N/A
 
 ## MISCELLANEOUS COMMENTS:
- < Anything you would like the grader to know >
+ N/A
--- main.py
+++ main.py
@@ -1,17 +1,59 @@
 import turtle
-
+import math
+from turtle import *
+import matplotlib.pyplot as plt
 ########### Your Code here ##############
 # You should only have functions here
 # If you have anything outside of a function, 
 # then you do not fully understand functions
 # and should review how they work or ask for help
+def test():
+  y = math.sin(math.radians(90))
+  print(y)
+test()
 
 
+def drawSineCurve():
+  for num in range(-360,360):   
+    for i in range(num):
+      turt_one = turtle.Turtle()
+      turt_one = turtle.down()
+      x = math.sin(math.radians(i))
+      turt_one = turtle.goto(num,num)
+      turt_one = turtle.speed(110)
+drawSineCurve()
 
+def setupWindow(mywindow=None):
+  sc = turtle.Screen()
+  sc.mode('world')
+  turtle.setworldcoordinates(-400,-400,400,400)
+setupWindow()
 
+def setupAxis(myturtle=None):
+  x = [-400, 0, 400]
+  y = [-400, 0, 400]
+  turtle.set(0,0)
+setupAxis()
 
+def drawCosineCurve():
+  for num in range(-360,360):   
+    for i in range(num):
+      turt_two = turtle.Turtle()
+      turt_two = turtle.down()
+      x = math.cos(math.radians(i))
+      turt_two = turtle.goto(num,num)
+      turt_two = turtle.speed(110)
+drawCosineCurve()
 
-
+def drawTangentCurve():
+  for num in range(-360,360):   
+    for i in range(num):
+      turt_three = turtle.Turtle()
+      turt_three = turtle.down()
+      x = math.tan(math.radians(i))
+      turt_three = turtle.goto(num,num)
+      turt_three = turtle.speed(110)
+drawTangentCurve()
 
 ##########  Do Not Alter Any Code Past Here ########
 def main():
@@ -20,7 +62,8 @@ def main():
     wn.tracer(5)
     dart = turtle.Turtle()
     dart.speed(0)
-    drawSineCurve(dart)
+    wn.exitonclick()
+
 
     #Part B
     setupWindow(wn)
@@ -30,4 +73,5 @@ def main():
     drawCosineCurve(dart)
     drawTangentCurve(dart)
     wn.exitonclick()
+
 main()
